d0a979b55fbe20da3414e3142a43750f8346ccfc
Mikael Paulsson <mikael.pal@gmail.com>
2022-06-01 21:05:11 +0200

    finished simple demo

--- async-demo/demo/Program.cs
+++ async-demo/demo/Program.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections.Concurrent;
+﻿namespace demo;
 
-namespace demo;
 class Program
 {
-
-    public static async Task Main()
+    public static void Main()
     {
-        
+        var thread = new Thread(Execute);
+        thread.Start();
+
+        //ThreadPool.QueueUserWorkItem(ExecuteUsingThreadPool);
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+    }
+
+    private static void Execute()
+    {
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+    }
+
+    private static void ExecuteUsingThreadPool(Object stateInfo)
+    {
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
     }
 }
